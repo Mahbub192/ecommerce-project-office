bc5634b980bf742768e923f642e8354f622a9070
unknown <mahabubaligub192@gmail.com>
2023-10-15 14:20:53 +0600

    user

--- pages/Dashboard/UserDashboard/SellProducts/SellProducts.js
+++ pages/Dashboard/UserDashboard/SellProducts/SellProducts.js
@@ -11,22 +11,57 @@ import {
 } from "react-icons/fa6";
 import { MdProductionQuantityLimits } from "react-icons/md";
 import ImageUploadForm from "./ImageUploadForm";
+import Swal from "sweetalert2";
+import AuthProvider from "@/pages/providers/AuthProvider";
 
 const SellProducts = () => {
+  const {user} = useContext(AuthProvider)
   const [ImageUrl, setImageUrl] = useState([]);
-  console.log(ImageUrl.length);
+  let email;
   const {
     register,
     handleSubmit,
     watch,
+    reset,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    
-  }
+  const onSubmit = async (data) => {
+    const productInfo = {
+      productName: data.name,
+      shopName: data.shopName,
+      location: data.location,
+      category: data.category,
+      phoneNumber: data.phoneNumber,
+      imageUrl: ImageUrl,
+      message: data.message,
+      status: "pending",
+      quantity: data.quantity,
+    };
+    console.log(productInfo);
+    const response = await fetch(`api/sellProductsAPI`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(productInfo),
+    });
+    const responseData = await response.json();
+    if (responseData.data.acknowledged) {
+      reset();
+      Swal.fire({
+        icon: "success",
+        title: "Product added successfully in database.",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
 
-  
+  useEffect(()=>{
+    email={user.?eamil}0
+  },[user])
+
   return (
     <div className="w-3/5 mx-auto bg-white p-10 shadow-2xl">
       <h1 className="text-2xl front-bold">Product Details</h1>
@@ -90,7 +125,7 @@ const SellProducts = () => {
                 <span className="pl-2">Product Category </span>
               </span>
             </label>
-            <select {...register("gender")} className="input input-bordered">
+            <select {...register("category")} className="input input-bordered">
               <option value="Health & Beauty">Health & Beauty</option>
               <option value="Men's & Boy's Fashion">
                 Men's & Boy's Fashion
@@ -108,9 +143,25 @@ const SellProducts = () => {
             </select>
           </div>
         </div>
-        <div></div>
+          
         {/* Phone Number and Location Input filed */}
         <div className="flex flex-wrap justify-around gap-6 mt-5">
+          <div className="form-control w-2/5">
+            <label className="label ">
+              <span className="label-text flex items-center">
+                {" "}
+                <MdProductionQuantityLimits />{" "}
+                <span className="pl-2">Product Quantity *</span>
+              </span>
+            </label>
+            <input
+              type="number"
+              {...register("quantity", { required: true })}
+              name="quantity"
+              placeholder="01"
+              className="input input-bordered"
+            />
+          </div>
           <div className="form-control w-2/5">
             <label className="label ">
               <span className="label-text flex items-center">
@@ -123,21 +174,6 @@ const SellProducts = () => {
               {...register("phoneNumber", { required: true })}
               name="phoneNumber"
               placeholder="01********"
-              className="input input-bordered"
-            />
-          </div>
-          <div className="form-control w-2/5">
-            <label className="label ">
-              <span className="label-text flex items-center">
-                {" "}
-                <FaLocationDot /> <span className="pl-2">Location</span>
-              </span>
-            </label>
-            <input
-              type="text"
-              {...register("location", { required: true })}
-              name="location"
-              placeholder="Location"
               className="input input-bordered"
             />
           </div>
@@ -156,9 +192,24 @@ const SellProducts = () => {
             </div>
           </div>
         </div>
+        <div className="form-control mx-10">
+            <label className="label ">
+              <span className="label-text flex items-center">
+                {" "}
+                <FaLocationDot /> <span className="pl-2">Location</span>
+              </span>
+            </label>
+            <input
+              type="text"
+              {...register("location", { required: true })}
+              name="location"
+              placeholder="Location"
+              className="input input-bordered"
+            />
+          </div>
 
         <div className="">
-          <div className="form-control  md:mx-10 mt-8">
+          <div className="form-control  md:mx-10 mt-4">
             <label className="label ">
               <span className="label-text flex items-center">
                 {" "}
@@ -176,7 +227,9 @@ const SellProducts = () => {
         </div>
         <button
           className={`mt-16 mx-auto border-2 px-10 py-2 text-lg md:text-xl font-bold ${
-            ImageUrl.length === 0 ? "bg-red-300 text-gray-300" : "bg-blue-500 hover:bg-blue-800 text-white"
+            ImageUrl.length === 0
+              ? "bg-red-300 text-gray-300"
+              : "bg-blue-500 hover:bg-blue-800 text-white"
           }  `}
           type="submit"
           disabled={ImageUrl.length === 0}
